058e61dc7c42258c51d208a8cbcc642ea1bba19a
kalyaniphad <kalyanishashikalaphad29@gmail.com>
2023-12-17 13:56:13 +0530

    method1() is created

--- src/main/java/com/mycompany/Hello.java
+++ src/main/java/com/mycompany/Hello.java
@@ -4,5 +4,17 @@ public class Hello {
 	
 	public static void main(String args[])
 	{
-		System.out.println("Hello kalyani");	}
+		
+		System.out.println("Hello kalyani");	
+		Hello h = new Hello();
+		h.method1();
+		
+	
+	}
+	
+	
+	void method1() {
+		
+		System.out.println("Hello");
+	}
 }
